e0db059a44161a3f70e0993e90643243b8187a18
shivam-op <84925754+shivam-op@users.noreply.github.com>
2021-06-29 15:38:52 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -145,11 +145,14 @@ def start(bot: Bot, update: Update, args: List[str]):
                                                                        url="t.me/SHIVAM9412")],
                                                                                    [InlineKeyboardButton(text="GROUP",
                                                                        url="https://t.me/SUPER_X_GROUP")
-                                                                                   InlineKeyboardButton(text="CREDITS",
-                                                                       url="https://t.me/LEGENDX22")],
-                                                                                   [InlineKeyboardButton(text="ADD SUPER-X TO YOUR GROUP",
+                                                                                   [InlineKeyboardButton(text="ADD SUPERX TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
-                                                                                 ]]))
+                                                                                   InlineKeyboardButton(text="CREDITS",
+                                                                       url="https://t.me/legendx22")
+                                                                                 ],
+                                                                                   [InlineKeyboardButton(text="ADD SUPERX TO YOUR GROUP",
+                                                                       url="t.me/{}?startgroup=true".format(bot.username)),
+                                                                                ]]))
 
     else:
         update.effective_message.reply_text("Yuss, I am Already ONline")
